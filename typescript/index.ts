3422998edbacc2e25c01f0ed3e79a24186f2083c
Luis Guerreiro <luiscvlh11@gmail.com>
2023-09-20 17:10:34 -0300

    feat[typescript]: adicionado queue

---
+++ typescript/index.ts
@@ -0,0 +1,43 @@
+/// <reference path="./linkedList.ts" />
+
+import assert from "assert";
+
+namespace NSQueue {
+    export class Queue {
+        queue: NSLinkedList.LinkedList = new NSLinkedList.LinkedList();
+
+        add(value: number) {
+            this.queue.add(value);
+        }
+
+        pop() {
+            const value = this.queue.get(0);
+            if (this.queue.remove(value)) {
+                return value;
+            }
+        }
+
+        get length(): number {
+            return this.queue.length;
+        }
+    }
+}
+
+const queue = new NSQueue.Queue();
+
+assert.throws(() => queue.pop(), Error);
+
+queue.add(10);
+queue.add(20);
+queue.add(30);
+
+assert.equal(queue.length, 3);
+
+assert.equal(queue.pop(), 10);
+assert.equal(queue.length, 2);
+
+assert.equal(queue.pop(), 20);
+assert.equal(queue.length, 1);
+
+assert.equal(queue.pop(), 30);
+assert.equal(queue.length, 0);
